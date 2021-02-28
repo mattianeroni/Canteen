9f00bf682e1252ffa7c2e67cff6d3d6f79b71912
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-02-28 23:35:09 +0100

    Add files via upload

---
+++ canteen/priority.py
@@ -0,0 +1,8 @@
+# This file contains the priority levels used in the simulation.
+#
+# Designed and developed by Mattia Neroni in March 2021.
+# Author contact: mattianeroni@yahoo.it
+EXTRAORDINARY = 1
+URGENT = 5
+MEDIUM = 7
+NORMAL = 10
---
+++ canteen/products.py
@@ -0,0 +1,37 @@
+# This file contains the products sold in the canteen.
+#
+# Designed and developed by Mattia Neroni in March 2021.
+# Author contact: mattianeroni@yahoo.it
+
+# Starters
+CAPRESE = 'caprese'
+COLDRICE = 'coldrice'
+HAM = 'ham'
+
+# Pizza
+PIZZA = 'pizza'
+
+# First dishes
+CARBONARA = 'carbonara'
+RAGU = 'ragu'
+RICE = 'rice'
+
+# Second dishes
+MEET = 'meet'
+FISH = 'fish'
+ROAST = 'roast'
+
+# Side dishes
+SALAD = 'salad'
+MAIS = 'mais'
+POTATOES = 'potatoes'
+
+# Sweets
+YOGURT = 'yogurt'
+CAKE = 'cake'
+FRUIT = 'fruit'
+
+# Drinks
+COKE = 'coke'
+WATER = 'water'
+BEER = 'beer'
